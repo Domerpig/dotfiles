4e769c2a9988e08ed9725ab9d2b65195f05d58c0
Dominik <dominik@linux.fritz.box>
2023-10-17 17:53:06 +0200

    scripts: fix setup script

--- .config/scripts/system_setup.sh
+++ .config/scripts/system_setup.sh
@@ -4,14 +4,15 @@
 if [[ $(lspci | rg VGA) == *"NVIDIA"* ]]; then
   zypper -n addrepo --refresh 'https://download.nvidia.com/opensuse/leap/$releasever' NVIDIA
 fi
-zypper -n addrepo https://download.opensuse.org/repositories/shells/openSUSE_Tumbleweed/shells.repo
+zypper -n addrepo https://download.opensuse.org/repositories/shells/openSUSE_Tumbleweed/shells.repo | echo 'a'
+
+zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
+zypper dup --from packman-essentials --allow-vendor-change | echo 'a'
 
 # Install Packages
 zypper -n install gcc-c++ git kitty gitui starship ripgrep helix polybar bottom flatpak spotify-easyrpm npm nushell
 
 flatpak install -y --noninteractive flathub com.logseq.Logseq
-
-spotify-easyrpm --quiet
 
 # Install language servers
 npm i -g bash-language-server vscode-langservers-extracted
