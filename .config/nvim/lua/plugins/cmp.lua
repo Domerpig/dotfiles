bf9dd30d945f175c9b85bebda77d77a80420b902
Domerpig <dominik.moos@proton.me>
2023-12-19 20:36:56 +0100

    nvim: fix luasnip bug

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -40,8 +40,12 @@ return {
 
     cmp.setup {
       snippet = {
+        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-          luasnip.lsp_expand(args.body) -- For `luasnip` users.
+          -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+          -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         end,
       },
       mapping = cmp.mapping.preset.insert{
