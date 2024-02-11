8efb82a3f4898c68d2a519a0e8f397031e639b98
Domerpig <dominik.moos@proton.me>
2024-02-11 13:51:15 +0100

    wez: initial config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.default_cursor_style = 'BlinkingBar'
+config.default_prog = { '/usr/bin/fish', '-l' }
+config.window_close_confirmation = 'NeverPrompt'
+config.keys = {
+  {
+    key = 'r',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ReloadConfiguration,
+  },
+}
+-- For example, changing the color scheme:
+config.color_scheme = 'Ayu Mirage'
+
+-- and finally, return the configuration to wezterm
+return config
