acf33dd4f3b8306a75a7fa1b3cb4e630f636a392
Domerpig <dominik.moos@proton.me>
2024-02-21 21:21:04 +0100

    fish: update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    starship init fish | source
-    alias ll="ls -l"
     alias dotfiles="git --git-dir=/home/dominik/.dotfiles/ --work-tree=/home/dominik/"
+
+    starship init fish | source
+    zoxide init fish | source
 end
