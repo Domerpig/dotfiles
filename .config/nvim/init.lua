4e87ce5a679c81b964c85d0af3f2a7525ebbae58
dominik <dominik@localhost.localdomain>
2024-06-25 20:54:43 +0200

    nvim: change cursor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,4 +3,6 @@ require("pluginmanager")
 require("options")
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme nightfox]])
-vim.cmd([[set guicursor=]])
+vim.api.nvim_create_autocmd('VimLeave', {
+  command = 'set guicursor=a:ver25,a:blinkwait700-blinkoff700-blinkon700-Cursor'
+})
