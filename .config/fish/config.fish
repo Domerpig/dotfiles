e79f75a5bb1a73e37d04b6f9153abbb1de861611
Dominik <dominik.moos@proton.me>
2024-07-19 17:54:13 +0200

    fish: update conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
-    alias dotfiles="git --git-dir=/home/dominik/.dotfiles/ --work-tree=/home/dominik/"
-
-    starship init fish | source
     zoxide init fish | source
+    starship init fish | source
+
+    # Aliases
+    alias cd="z"
+    alias dotfiles="git --git-dir=/home/dominik/.dotfiles/ --work-tree=/home/dominik/"
+    alias cargo_update="cargo install --locked --force $(cargo install --list | grep -Eo '^[^ ]+')"
 end
+
