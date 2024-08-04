d1ff23bc243cd6bf8b52d3102efb55f57207ec47
Dominik Moos <dominik.moos@proton.me>
2024-08-04 18:29:02 +0200

    cleanup

--- .config/scripts/session_river.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-# Needs to be executed on river startup, because other XDG_PORTAL is not working
-
-export XDG_CURRENT_DESKTOP=river
-export XDG_SESSION_TYPE=wayland
-
-systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
-
