90474eb80f25f5c22f251cd084f5a2b4267412cc
Dominik Moos <dominik.moos@proton.me>
2024-08-16 14:00:46 +0200

    update setup script

--- .config/scripts/system_setup.sh
+++ .config/scripts/system_setup.sh
@@ -1,15 +1,20 @@
-#!/bin/env bash
+#!/bin/bash
+
+xbps-install -Syu
 
 # Add necessary repos
 if [[ $(lspci | grep VGA) == *"NVIDIA"* ]]; then
     xbps-install -y void-repo-nonfree void-repo-multilib
+    xbps-install -Sy
+    xbps-install linux-headers nvidia
+else
+    xbps-install linux-firmware-amd vulkan-loader mesa-vulkan-radeon amdvlk mesa-dri
 fi
 
-xbps-install -Syu
 # Install Packages
-xbps-install -y gcc gdb curl linux-headers nvidia seatd starship ripgrep neovim fish-shell waybar  \
-    wmenu foot lswlt kanshi noto-fonts-ttf river fzf zoxide git unzip elogind polkit dunst tokei   \
-    wl-clipboard firefox dumb_runtime_dir dumb_runtime_dir dbus nextcloud-client
+xbps-install -y gcc gdb curl seatd starship ripgrep neovim fish-shell waybar wmenu foot lswlt      \
+    kanshi noto-fonts-ttf river fzf zoxide git unzip elogind polkit dunst tokei wl-clipboard       \
+    firefox dumb_runtime_dir dumb_runtime_dir dbus nextcloud-client zellij bottom
 
 usermod -aG _seatd dominik
 
