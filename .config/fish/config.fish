d02465e79dc49e4d0de0fee547c4a3d04ffdafe7
Dominik Moos <dominik.moos@proton.me>
2024-08-29 20:07:22 +0200

    fish: add pyenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,13 @@
 if status is-interactive
     zoxide init fish | source
     starship init fish | source
+    pyenv init - | source
 
     # Aliases
+    alias sudo="sudo "
     alias cd="z"
     alias dotfiles="git --git-dir=/home/dominik/.dotfiles/ --work-tree=/home/dominik/"
     alias cargo_update="cargo install --locked --force $(cargo install --list | grep -Eo '^[^ ]+')"
+    # eval (zellij setup --generate-auto-start fish | string collect)
 end
 
