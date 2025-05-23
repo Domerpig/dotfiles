069a4b73dc945560cafa3eb52f3627396a09b80f
Domerpig <domerpig@proton.me>
2025-05-23 21:19:40 +0200

    fish: update conf

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,4 +1,7 @@
 if status is-interactive
+    # disable greeting
+    set -U fish_greeting
+
     zoxide init fish | source
     starship init fish | source
     pyenv init - | source
@@ -6,6 +9,7 @@ if status is-interactive
     # Aliases
     alias cd="z"
     alias cargo_update="cargo install --locked --force $(cargo install --list | grep -Eo '^[^ ]+')"
+    alias ce="chezmoi edit"
     # eval (zellij setup --generate-auto-start fish | string collect)
 end
 
