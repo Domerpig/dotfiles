0dafa11285e147c67d55560bafdaab68c90a6d96
Domerpig <domerpig@proton.me>
2025-06-11 21:38:50 +0200

    fish: add aliases

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -10,9 +10,14 @@ if status is-interactive
     alias cd="z"
     alias cargo_update="cargo install --locked --force $(cargo install --list | grep -Eo '^[^ ]+')"
     alias ce="chezmoi edit"
+    alias ca="chezmoi apply -v"
+    alias ccd="chezmoi cd"
     alias gc="git commit -m"
     alias ga="git add"
     alias gs="git status"
+    alias gp="git pull"
+    alias gP="git push"
+    alias gd="git diff"
     # eval (zellij setup --generate-auto-start fish | string collect)
 end
 
