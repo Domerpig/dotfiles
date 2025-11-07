b3c2879262f80043658b7919f5ecb014a2fb91bc
Domerpig <domerpig@proton.me>
2025-11-07 22:27:01 +0100

    nvim: add harpoon keybinds

--- private_dot_config/nvim/lua/plugins/harpoon.lua
+++ private_dot_config/nvim/lua/plugins/harpoon.lua
@@ -11,5 +11,17 @@ return {
     vim.keymap.set("n", "<C-e>", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
+    vim.keymap.set("n", "<C-h>", function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set("n", "<C-t>", function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set("n", "<C-n>", function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set("n", "<C-l>", function()
+      harpoon:list():select(4)
+    end)
   end,
 }
